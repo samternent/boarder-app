0f6c0658797ef5ae7efda443a370f774f7bbdc89
Sam Ternent <sam.ternent@gmail.com>
2016-01-11 13:13:49 +0000

    try port again

--- src/server/app.js
+++ src/server/app.js
@@ -14,7 +14,7 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 var routes = require('./routes')(app);
 
-var port = process.env.PORT || 5000;
+var port = server.listen(process.env.PORT || 3000);
 
 app.listen(port, function() {
   console.log('Node app is running on port', port);
